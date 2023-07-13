033ef0ec38385f6fb58889450f63c2151ec72724
Fraol Taye <fraoltaye2@gmail.com>
2023-07-13 06:01:28 -0700

    First c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#inlude<stdio.h>
 /**
  * main - Entry point
  *
@@ -9,7 +9,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Sizr of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
-}
+} 
