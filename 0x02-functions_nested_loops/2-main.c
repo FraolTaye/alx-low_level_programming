0217962ae082eada3c55158067ff6c84cbbcbf2b
Fraol Taye <fraoltaye2@gmail.com>
2023-07-18 04:59:57 -0700

    third c

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,12 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+int print_alphabet_x10(void)
 {
-    Print_alphabet_x10();
+    print_alphabet_x10();
     return (0);
 }
