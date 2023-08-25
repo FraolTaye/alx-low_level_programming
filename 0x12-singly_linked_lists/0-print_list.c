e5ae7fcc0efdd5bcd74f2571dba82878aa0f47b2
Fraol Taye <fraoltaye2@gmail.com>
2023-08-25 04:01:44 -0700

    c twenty

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+* print_list - in the linked list it has elements and this will print that. 
+* @m: the lists name 
+* Return: nodes number.
+*/
+size_t print_list(const list_t *m)
+{
+   int y = 0;
+   while (m)
+   {
+       if (m->str == NULL)
+       {
+           printf("[0] (nil)\n");
+       }
+       else
+       {
+       printf("[%d] %s\n", m->len, m->str);
+       }
+       y++;
+       m = m->next;
+   }
+   return (y);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+this project is named  C singly linked lists
+It has four tasks and two additional extra tasks
+
