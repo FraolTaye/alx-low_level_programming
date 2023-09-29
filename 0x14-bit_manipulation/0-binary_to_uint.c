25c8b63d03ad91f1b944a492f5dfbb0f24f57cdf
FraolTaye <fraoltaye2@gmail.com>
2023-09-29 09:26:38 -0700

    Bitman

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uint -supposed to convert bit num to unsigned integer
+ * @bi: the bin number
+ * Return: the num converted
+ */
+unsigned int binary_to_uint(const char *bi)
+{
+	int k;
+	unsigned int dec_val = 0;
+
+	while (!bi)
+		return (0);
+
+	for (k = 0; bi[k]; k++)
+	{
+		if (bi[k] < '0' || bi[k] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (bi[k] - '0');
+	}
+
+	return (dec_val);
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+This is project 0x14 c which is about bit manipulation
+it has an overall of eight project works
