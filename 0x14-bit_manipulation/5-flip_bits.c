abcd708b3d8f5f65659e18b44247f85d0e810396
FraolTaye <fraoltaye2@gmail.com>
2023-09-29 10:21:36 -0700

    Bitman

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - supposed to count the amount of bits
+ * @fn: the num one
+ * @sn: the num on two
+ * Return: the bit num
+ */
+unsigned int flip_bits(unsigned long int fn, unsigned long int sn)
+{
+	int r, snt = 0;
+	unsigned long int dex;
+	unsigned long int mul = fn ^ sn;
+
+	for (r = 63; r >= 0; r--)
+	{
+		dex = mul >> r;
+		if (dex & 1)
+			snt++;
+	}
+	return (snt);
+}
+
