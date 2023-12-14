acf4084c610d5b973edd20aefa43c38e554aa32f
FraolTaye <fraoltaye2@gmail.com>
2023-12-14 02:19:08 -0800

    third

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,8 @@
 /**
 * set_bit - supposde to arrange any to any index
 * @pn: the pointer of the num
-* @bidx: the setted bit of value 1 * Return: if succes 1 and -1 if fail
+* @bidx: the setted bit of value 1
+* Return: if succes 1 and -1 if fail
 */
 int set_bit(unsigned long int *pn, unsigned int bidx)
 {
