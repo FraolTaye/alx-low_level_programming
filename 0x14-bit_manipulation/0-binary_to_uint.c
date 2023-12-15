efb19c09293849fbacdec06211cfa0d1a1cd2f23
FraolTaye <fraoltaye2@gmail.com>
2023-12-15 00:24:37 -0800

    Onetask

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,19 +6,16 @@
  */
 unsigned int binary_to_uint(const char *bi)
 {
-        int k;
-        unsigned int dec_val = 0;
+	int k;
+	unsigned int dec_val = 0;
 
-        while (!bi)
-                return (0);
-
-        for (k = 0; bi[k]; k++)
-        {
-                if (bi[k] < '0' || bi[k] > '1')
-                        return (0);
-                dec_val = 2 * dec_val + (bi[k] - '0');
-        }
-
-        return (dec_val);
+	while (!bi)
+		return (0);
+	for (k = 0; bi[k]; k++)
+	{
+		if (bi[k] < '0' || bi[k] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (bi[k] - '0');
+	}
+	return (dec_val);
 }
-
