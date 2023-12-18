1969d226e2fd5d89cc6482a155bb69599c82f15c
FraolTaye <fraoltaye2@gmail.com>
2023-12-18 12:19:22 -0800

    fifteen

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - suppose to be a file creator
+ * @filename: the file pointer creator
+ * @text_content: the stirng pointre
+ * Return: -1 when function fail and 1 for else
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int myf = 0;
+	int out = 0;
+	int coutl = 0;
+
+	if (!filename)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (coutl = 0; text_content[coutl];)
+			coutl++;
+	}
+	myf = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	out = write(myf, text_content, coutl);
+	if (myf == -1 || out == -1)
+		return (-1);
+	close(myf);	
+	return (1);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+The project of this week is 0x15. C  File I/O
+it has a total of 5 project works
